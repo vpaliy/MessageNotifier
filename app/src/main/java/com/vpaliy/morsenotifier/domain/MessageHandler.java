27206c0f6b9d9aa1e053388b14b731c9c8a0b015
Vasya Paliy <vpaliy97@gmail.com>
2017-03-10 12:55:51 -0500

    add model

--- app/src/main/java/com/vpaliy/morsenotifier/domain/MessageHandler.java
+++ app/src/main/java/com/vpaliy/morsenotifier/domain/MessageHandler.java
@@ -7,10 +7,12 @@ import android.telephony.SmsMessage;
 import com.vpaliy.morsenotifier.R;
 import com.vpaliy.morsenotifier.domain.converter.Converter;
 import com.vpaliy.morsenotifier.domain.converter.MorseConverter;
+import com.vpaliy.morsenotifier.domain.model.ConvertModel;
 import com.vpaliy.morsenotifier.domain.wrapper.TransformWrapper;
 import com.vpaliy.morsenotifier.domain.wrapper.VibratorWrapper;
 import com.vpaliy.morsenotifier.utils.SHWrapper;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MessageHandler {
@@ -26,7 +28,6 @@ public class MessageHandler {
     @NonNull
     private SHWrapper shWrapper;
 
-    private static  MessageHandler INSTANCE;
 
     private MessageHandler(@NonNull Context context,@NonNull List<SmsMessage> smsMessageList) {
         this.smsMessageList=smsMessageList;
@@ -36,14 +37,7 @@ public class MessageHandler {
     }
 
     public static MessageHandler buildHandler(@NonNull Context context, @NonNull List<SmsMessage> smsMessageList) {
-        if(INSTANCE==null) {
-            synchronized (MessageHandler.class) {
-                if(INSTANCE==null) {
-                    INSTANCE=new MessageHandler(context, smsMessageList);
-                }
-            }
-        }
-        return INSTANCE;
+        return new MessageHandler(context,smsMessageList);
     }
 
     public void handle() {
@@ -54,7 +48,13 @@ public class MessageHandler {
 
 
     private Converter<? extends TransformWrapper> provideWithConverter(TransformWrapper wrapper) {
-        return new MorseConverter<>(wrapper);
+        List<ConvertModel> convertModelList=new LinkedList<>();
+        for(SmsMessage smsMessage:smsMessageList) {
+            ConvertModel.Builder builder=new ConvertModel.Builder(smsMessage.getMessageBody(),
+                smsMessage.getOriginatingAddress()).frequency(1).volume(1);
+            convertModelList.add(builder.build());
+        }
+        return new MorseConverter<>(wrapper,convertModelList);
     }
 
     private TransformWrapper provideWithWrapper() {
